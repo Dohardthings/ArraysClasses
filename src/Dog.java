60e15ec255fd7f3f3c0baf948a1c29c920385812
Daniel McBryde <mcbryded@gmail.com>
2016-08-29 14:09:43 -0500

    starting assignment

---
+++ src/Dog.java
@@ -0,0 +1,20 @@
+/**
+ * Created by danielmcbryde on 8/29/16.
+ */
+
+//this is a class that makes a dog
+
+public class Dog {
+
+    static int age = 12;
+    String breed;
+    int numberOfLegs;
+    String[] tricksKnown;
+    boolean isHouseBroken;
+
+
+
+
+}
+
+
---
+++ src/User.java
@@ -0,0 +1,18 @@
+/**
+ * Created by danielmcbryde on 8/29/16.
+ */
+
+// this creates a user for the website
+
+
+public class User {
+    String name;
+    boolean isAdmin = false;
+    String[] participatedInTheseHunts;
+    boolean hasWonAHunt;
+    int moneyWon;
+    int rank;
+
+
+
+}
